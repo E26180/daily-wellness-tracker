2e2c6980be83d43eb752046ddcb85f38e9bd5737
Ahmet Ege Baykoz <gnn534@york.ac.uk>
2026-07-02 12:40:44 +0100

    Add main Python file

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to Daily Wellness Tracker")
+
+water = input("How much water did you drink today? ")
+exercise = input("Did you exercise today? ")
+
+print("\nDaily Wellness Summary")
+print("Water intake:", water)
+print("Exercise:", exercise)
